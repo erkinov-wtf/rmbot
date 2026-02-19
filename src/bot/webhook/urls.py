5d635daf95cc283fc3c4c18ea841888c876b8b4d
Rakhmatilla Erkinov <erkinovit@gmail.com>
2026-02-19 15:26:51 +0500

    feat(bot): update webhook path configuration and simplify URL routing

--- src/bot/webhook/urls.py
+++ src/bot/webhook/urls.py
@@ -5,5 +5,5 @@ from bot.webhook import views
 app_name = "bot_webhook"
 
 urlpatterns = [
-    path("webhook/", views.telegram_webhook, name="telegram_webhook"),
+    path("", views.telegram_webhook, name="telegram_webhook"),
 ]
--- src/config/urls/base.py
+++ src/config/urls/base.py
@@ -2,11 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from config.settings.base import BOT_WEBHOOK_PATH
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("api.url_router"), name="url_router"),
-    path("bot/", include("bot.webhook.urls", namespace="bot_webhook")),
+    path(BOT_WEBHOOK_PATH, include("bot.webhook.urls", namespace="bot_webhook")),
     *static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT),
     *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
 ]
